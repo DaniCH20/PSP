34e0df025614eceb95c5b77aa4639e46eb814ea4
Daniel Edgar Caballero <danieledgar.caballero@elorrieta-errekamari.com>
2024-10-04 11:23:57 +0200

    Segundo commit

--- PSP/src/Ejer4/DetonadorConRetardo.java
+++ PSP/src/Ejer4/DetonadorConRetardo.java
@@ -1,21 +1,22 @@
 package Ejer4;
 
-public class DetonadorConRetardo extends Thread{
-	private int n;
+public class DetonadorConRetardo extends Thread {
+    private int contador;
 
-	public DetonadorConRetardo(String nom, int n) {
-		super(nom);
-		this.n = n;
-	}
+    public DetonadorConRetardo(String nom, int contador) {
+        super(nom);
+        this.contador = contador;
+    }
 
-	public void run() {
-		for (int i = 1; i < n; i++) {
-			System.out.println(getName() + ":" + i);
-			try {
-				sleep(1000);
-			} catch (InterruptedException ignore) {
-			}
-		}
-		System.out.println("Fin bucle" + getName());
-	}
+    public void run() {
+        while (contador > 0) {
+            System.out.println(getName() + ": " + contador);
+            contador--; // Reducir el contador
+            try {
+                sleep(1000); // Espera de 1 segundo
+            } catch (InterruptedException ignore) {
+            }
+        }
+        System.out.println("Fin bucle " + getName());
+    }
 }
--- PSP/src/Ejer4/Principal.java
+++ PSP/src/Ejer4/Principal.java
@@ -2,24 +2,28 @@ package Ejer4;
 
 public class Principal {
 
-	public static void main(String[] args) {
-		DetonadorConRetardo d1=new DetonadorConRetardo("Bomba1",5);
-		DetonadorConRetardo d2=new DetonadorConRetardo("Bomba2",5);
-		DetonadorConRetardo d3=new DetonadorConRetardo("Bomba3",5);
-		DetonadorConRetardo d4=new DetonadorConRetardo("Bomba4",5);
-		d1.start();
-		d2.start();
-		d3.start();
-		d4.start();
-		try {
-			d1.join();
-			d2.join(); // Interrumpe el proceso durnate 8 segundos
-			d3.join();
-			d4.join();
-		} catch (InterruptedException e) {
+    public static void main(String[] args) {
+        DetonadorConRetardo d1 = new DetonadorConRetardo("Bomba1", 5);
+        DetonadorConRetardo d2 = new DetonadorConRetardo("Bomba2", 4);
+        DetonadorConRetardo d3 = new DetonadorConRetardo("Bomba3", 3);
+        DetonadorConRetardo d4 = new DetonadorConRetardo("Bomba4", 6);
 
-			System.out.printf("Fin del Programa");
-		}
-	}
+        d1.start();
+        d2.start();
+        d3.start();
+        d4.start();
 
+        try {
+            d1.join();
+            d2.join();
+            d3.join();
+            d4.join();
+        } catch (InterruptedException e) {
+            // Manejo de la excepción si ocurre
+            System.out.println("Se interrumpió un hilo");
+        }
+
+        // Mensaje de fin del programa
+        System.out.println("Fin del Programa");
+    }
 }
